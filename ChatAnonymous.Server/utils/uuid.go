af3fb80b7e04aa5d6769aeee7c277d7725c478fd
Paulo Gonçalves <paulomontealtomac@outlook.com>
2025-12-06 18:49:59 -0300

    feat(utils) function uuidnew now contains error message

--- ChatAnonymous.Server/utils/uuid.go
+++ ChatAnonymous.Server/utils/uuid.go
@@ -5,24 +5,25 @@ import (
 	"fmt"
 )
 
-func NewUUID() string {
+func NewUUID() (string, error) {
 	u := make([]byte, 16)
+
 	_, err := rand.Read(u)
 	if err != nil {
-		return ""
+		return "", err
 	}
 
-	// Versão 4 (aleatória)
 	u[6] = (u[6] & 0x0f) | 0x40
 
-	// Variante (RFC 4122)
 	u[8] = (u[8] & 0x3f) | 0x80
 
-	return fmt.Sprintf("%08x-%04x-%04x-%04x-%012x",
+	uuid := fmt.Sprintf("%x-%x-%x-%x-%x",
 		u[0:4],
 		u[4:6],
 		u[6:8],
 		u[8:10],
 		u[10:16],
 	)
+
+	return uuid, nil
 }
